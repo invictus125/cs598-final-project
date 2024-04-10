e07e906a5b60856487be6ef07ce896c52fbc05f3
Ryan David <invictus.immortal@gmail.com>
2024-04-10 02:58:18 +0000

    Added resnet

--- src/model.py
+++ src/model.py
@@ -1,13 +1,16 @@
 import torch
 from torch import nn
 
+
+# TODO: encoder layer
+
+
 class ResidualBlock(nn.Module):
-  def __init__(self, dim_in, kernel_size, stride):
+  def __init__(self, dim_in, kernel_size=15, stride=1):
     super(ResidualBlock, self).__init__()
     self.bn1 = nn.BatchNorm1d(dim_in)
     self.act1 = nn.ReLU()
     self.do = nn.Dropout()
-    # TODO: verify what they did for convolutions
     self.conv1 = nn.Conv1d(dim_in, dim_in, kernel_size, stride)
     self.bn2 = nn.BatchNorm1d(dim_in)
     self.act2 = nn.ReLU()
@@ -30,23 +33,43 @@ class ResidualBlock(nn.Module):
 class FlattenAndLinearBlock(nn.Module):
   def __init__(self, dim_in, dim_out):
     super(FlattenAndLinearBlock, self).__init__()
-    self.fc1 = nn.Linear(dim_in, dim_in)
-    self.fc2 = nn.Linear(dim_in, dim_out)
-    self.act = nn.Sigmoid()
+    self.fc = nn.Linear(dim_in, dim_out)
 
   
   def forward(self, x):
     x_hat = torch.flatten(x)
-    x_hat = self.fc1(x_hat)
-    x_hat = self.fc2(x_hat)
-    y = self.act(x_hat)
-
-    return y
+    x_hat = self.fc(x_hat)
+    
+    return x_hat
 
 
-def create_1d_residual(dim_in, kernel_size, stride=1):
-  return ResidualBlock(dim_in, kernel_size, stride)
+class WaveformResNet(nn.Module):
+  def __init__(
+    self,
+    name,
+    input_shape,
+    output_size,
+    num_residuals=12,
+    filter_size=15,
+    stride=1
+  ):
+    self.encoder = None # TODO
+    self.res_in_dim = None # TODO - this should be the encoder's out dimension
+
+    self.residuals = []
+    for i in range(num_residuals):
+      self.residuals.append(ResidualBlock((self.res_in_dim * (i+1)), filter_size, stride))
+
+    self.fl_ln = FlattenAndLinearBlock((self.res_in_dim * num_residuals), output_size)
+
+  
+  def forward(self, x):
+    x_hat = self.encoder(x)
+
+    for i in range(len(self.residuals)):
+      x_hat = self.residuals[i](x_hat)
+
+    return self.fl_ln(x_hat)
 
 
-def create_flatten_and_linear(dim_in, dim_out):
-  return FlattenAndLinearBlock(dim_in, dim_out)
+# TODO: Create model with optional resnets for each input type and appropriate FC layers
