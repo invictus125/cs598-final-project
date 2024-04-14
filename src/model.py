32898080c6a4ff7d2afa44d05f9ae019fd3f8f45
Ryan David <invictus.immortal@gmail.com>
2024-04-14 03:14:09 +0000

    Model change - update resnet dimensions

--- src/model.py
+++ src/model.py
@@ -14,15 +14,22 @@ class EncoderBlock(nn.Module):
 
 
 class ResidualBlock(nn.Module):
-  def __init__(self, dim_in, kernel_size=15, stride=1):
+  def __init__(
+    self,
+    in_channels,
+    out_channels,
+    size_down,
+    kernel_size,
+    stride=1
+  ):
     super(ResidualBlock, self).__init__()
-    self.bn1 = nn.BatchNorm1d(dim_in)
+    self.bn1 = nn.BatchNorm1d(in_channels)
     self.act1 = nn.ReLU()
     self.do = nn.Dropout()
-    self.conv1 = nn.Conv1d(dim_in, dim_in, kernel_size, stride)
-    self.bn2 = nn.BatchNorm1d(dim_in)
+    self.conv1 = nn.Conv1d(in_channels, in_channels, kernel_size, stride)
+    self.bn2 = nn.BatchNorm1d(in_channels)
     self.act2 = nn.ReLU()
-    self.conv2 = nn.Conv1d(dim_in, dim_in, kernel_size, stride)
+    self.conv2 = nn.Conv1d(in_channels, out_channels, kernel_size, stride)
 
     self.seq = nn.Sequential(
       self.bn1,
@@ -36,13 +43,6 @@ class ResidualBlock(nn.Module):
 
 
   def forward(self, x):
-    # x_hat = self.bn1(x)
-    # x_hat = self.act1(x_hat)
-    # x_hat = self.do(x_hat)
-    # x_hat = self.conv1(x_hat)
-    # x_hat = self.bn2(x_hat)
-    # x_hat = self.act2(x_hat)
-    # x_hat = self.conv2(x_hat)
     x_hat = self.seq(x)
 
     # Concat raw input with convolved for skip connection
@@ -67,20 +67,185 @@ class WaveformResNet(nn.Module):
     self,
     input_shape,
     output_size,
-    num_residuals=12,
-    filter_size=15,
-    stride=1
+    data_type
   ):
     super(WaveformResNet, self).__init__()
     self.encoder = EncoderBlock(input_shape, 15, 1)
     self.res_in_dim = input_shape
     self.output_size = output_size
 
-    self.residuals = []
-    for i in range(num_residuals):
-      self.residuals.append(ResidualBlock((self.res_in_dim * (i+1)), filter_size, stride))
+    if data_type not in ['abp', 'ecg', 'eeg']:
+      raise ValueError('Invalid data type. Must be one of [abp, ecg, eeg]')
 
-    self.fl_ln = FlattenAndLinearBlock((self.res_in_dim * num_residuals), output_size)
+    # Set up configurations for residual blocks
+    residual_configs = []
+    linear_block_input_length = -1
+    if data_type in ['abp', 'ecg']:
+      residual_configs = [
+        {
+          'kernel_size': 15,
+          'in_channels': 1,
+          'out_channels': 2,
+          'size_down': True,
+        },
+        {
+          'kernel_size': 15,
+          'in_channels': 2,
+          'out_channels': 2,
+          'size_down': False,
+        },
+        {
+          'kernel_size': 15,
+          'in_channels': 2,
+          'out_channels': 2,
+          'size_down': True,
+        },
+        {
+          'kernel_size': 15,
+          'in_channels': 2,
+          'out_channels': 2,
+          'size_down': False,
+        },
+        {
+          'kernel_size': 15,
+          'in_channels': 2,
+          'out_channels': 2,
+          'size_down': True,
+        },
+        {
+          'kernel_size': 15,
+          'in_channels': 2,
+          'out_channels': 4,
+          'size_down': False,
+        },
+        {
+          'kernel_size': 7,
+          'in_channels': 4,
+          'out_channels': 4,
+          'size_down': True,
+        },
+        {
+          'kernel_size': 7,
+          'in_channels': 4,
+          'out_channels': 4,
+          'size_down': False,
+        },
+        {
+          'kernel_size': 7,
+          'in_channels': 4,
+          'out_channels': 4,
+          'size_down': True,
+        },
+        {
+          'kernel_size': 7,
+          'in_channels': 4,
+          'out_channels': 6,
+          'size_down': False,
+        },
+        {
+          'kernel_size': 7,
+          'in_channels': 4,
+          'out_channels': 6,
+          'size_down': True,
+        },
+        {
+          'kernel_size': 7,
+          'in_channels': 6,
+          'out_channels': 6,
+          'size_down': False,
+        },
+      ]
+      linear_block_input_length = 496
+    else:
+      residual_configs = [
+        {
+          'kernel_size': 7,
+          'in_channels': 1,
+          'out_channels': 2,
+          'size_down': True,
+        },
+        {
+          'kernel_size': 7,
+          'in_channels': 2,
+          'out_channels': 2,
+          'size_down': False,
+        },
+        {
+          'kernel_size': 7,
+          'in_channels': 2,
+          'out_channels': 2,
+          'size_down': True,
+        },
+        {
+          'kernel_size': 7,
+          'in_channels': 2,
+          'out_channels': 2,
+          'size_down': False,
+        },
+        {
+          'kernel_size': 7,
+          'in_channels': 2,
+          'out_channels': 2,
+          'size_down': True,
+        },
+        {
+          'kernel_size': 7,
+          'in_channels': 2,
+          'out_channels': 4,
+          'size_down': False,
+        },
+        {
+          'kernel_size': 3,
+          'in_channels': 4,
+          'out_channels': 4,
+          'size_down': True,
+        },
+        {
+          'kernel_size': 3,
+          'in_channels': 4,
+          'out_channels': 4,
+          'size_down': False,
+        },
+        {
+          'kernel_size': 3,
+          'in_channels': 4,
+          'out_channels': 4,
+          'size_down': True,
+        },
+        {
+          'kernel_size': 3,
+          'in_channels': 4,
+          'out_channels': 6,
+          'size_down': False,
+        },
+        {
+          'kernel_size': 3,
+          'in_channels': 6,
+          'out_channels': 6,
+          'size_down': True,
+        },
+        {
+          'kernel_size': 3,
+          'in_channels': 6,
+          'out_channels': 6,
+          'size_down': False,
+        },
+      ]
+      linear_block_input_length = 120
+
+    self.residuals = []
+    # Build residuals
+    for i in range(12):
+      self.residuals.append(
+        ResidualBlock(
+          size_down=residual_configs[i]['size_down'],
+          in_channels=residual_configs[i]['in_channels'],
+          out_channels=residual_configs[i]['out_channels'],
+          kernel_size=residual_configs[i]['kernel_size'],
+        )
+      )
+
+    self.fl_ln = FlattenAndLinearBlock(linear_block_input_length, output_size)
 
   
   def forward(self, x):
